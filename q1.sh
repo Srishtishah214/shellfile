2520005cded9cbed549f1c25f1f644593b70e20b
peakcipher <maitrey.sharma@niser.ac.in>
2019-08-09 08:26:46 -0400

    Done

---
+++ q1.sh
@@ -0,0 +1,6 @@
+echo "What is your name?"
+read name
+mkdir $name
+touch $name/$name.cpp
+echo "Folder created"
+
---
+++ q2.sh
@@ -0,0 +1,9 @@
+echo "Enter your name"
+read name
+mkdir $name
+touch $name/"$name"1.cpp
+touch $name/"$name"2.cpp
+touch $name/"$name"3.cpp
+touch $name/"$name"4.cpp
+touch $name/"$name"5.cpp
+echo "Files created "
